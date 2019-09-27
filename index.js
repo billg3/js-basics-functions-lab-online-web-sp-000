a659525dbdf07188dbcb544ac4d649ad45a750ee
Bill G <gbilly3@gmail.com>
2019-09-27 18:16:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 // Code your solution in this file!
-function distnceFromqInBlocks(someValue){
+function distanceFromHqInBlocks(Value){
+if (value > 42 )
+return 
+then
+
+}
+
+
+function distanceFromHqInFeet (Value) {
+blocks =  distanceFromHqInBlocks(Value);
 
 }
